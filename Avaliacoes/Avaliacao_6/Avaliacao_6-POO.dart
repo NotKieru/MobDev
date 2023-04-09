62e114490e8757897bb74c62b608bfc95d9f537d
IsequielNascimento <isequielnascimento@gmail.com>
2023-04-09 14:59:52 -0300

    resolução das Atividades 06, sobre Programação Orientada a Objetos.

---
+++ Avaliacoes/Avaliacao_6/Avaliacao_6-POO.dart
@@ -0,0 +1,63 @@
+void main(){
+
+  print("\n 01)O que é POO?");
+print(" b) Um paradigma de programação");
+
+print("\n 02)Qual a principal vantagem da POO?");
+print(" a) 0)Código mais fácil de entender e manter");
+
+print("\n 03)O que é uma classe em POO?");
+print(" b) Uma estrutura de dados que contém propriedades e métodos");
+
+print("\n 04)O que é um objeto em POO?");
+print(" b) Uma estrutura de dados que contém propriedades e métodos");
+
+print("\n 05)O que é herança em POO?");
+print(" a) Uma forma de reutilizar código de uma classe existente em uma nova classe");
+
+print("\n 06)O que é polimorfismo em POO?");
+print(" b) A capacidade de uma classe de assumir várias formas");
+
+print("\n 07)O que é encapsulamento em POO?");
+print(" a) A capacidade de esconder a implementação interna de uma classe");
+
+print("\n 08)O que é uma interface em POO?");
+print(" a) Um conjunto de métodos e propriedades que uma classe deve implementar");
+
+print("\n 09)O que é composição em POO?");
+print(" d) Uma forma de criar um objeto composto por outros objetos");
+
+print("\n 10) que é agregação em POO?");
+print(" a) Uma forma de criar um objeto composto por outros objetos");
+
+print("\n 11)Qual é o principal benefício da POO?");
+print(" a) Maior modularidade e reutilização de código.");
+
+print("\n 12)O que é uma classe em Dart?");
+print(" d) Um modelo para criar objetos.");
+
+print("\n 13)Como se define uma classe em Dart?");
+print(" a) class NomeDaClasse { }");
+
+print("\n 14)O que é um objeto em Dart?");
+print(" b) Uma instância de uma classe.");
+
+print("\n 15)O que é o construtor de uma classe em Dart?");
+print(" a) Um método especial que é executado quando um objeto é criado.");
+
+print("\n 16)O que é a herança em POO?");
+print(" a) Uma técnica que permite criar novas classes a partir de classes existentes.");
+
+print("\n 17)O que é o polimorfismo em POO?");
+print(" a) Uma técnica que permite que objetos de diferentes classes sejam tratados de maneira semelhante.");
+
+print("\n 18)Qual é a diferença entre uma classe abstrata e uma classe concreta em Dart?");
+print(" a) Uma classe abstrata não pode ser instanciada diretamente, enquanto uma classe concreta pode ser.");
+
+print("\n 19)O que é uma interface em Dart?");
+print(" a) Um tipo especial de classe que define um conjunto de métodos que uma classe deve implementar.");
+
+print("\n 20)O que é um relacionamento de associação em um diagrama de classes em POO?");
+print(" b) É quando uma classe usa a funcionalidade de outra classe sem herdar seus atributos e métodos.");
+
+}
